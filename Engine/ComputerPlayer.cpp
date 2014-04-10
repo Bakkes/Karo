7866803001bf16fa5d277c49e333b969b5b1a5a8
Johnnei <johnnei07@live.nl>
2014-04-10 15:26:03 +0200

    Added class for the computer player

---
+++ Engine/ComputerPlayer.cpp
@@ -0,0 +1,13 @@
+#include "ComputerPlayer.h"
+
+namespace engine {
+
+	ComputerPlayer::ComputerPlayer(void)
+	{
+	}
+
+
+	ComputerPlayer::~ComputerPlayer(void)
+	{
+	}
+}
---
+++ Engine/ComputerPlayer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "IComputerPlayer.h"
+
+namespace engine {
+
+	class ComputerPlayer : public IComputerPlayer {
+	public:
+		ComputerPlayer();
+		~ComputerPlayer();
+
+	private:
+	};
+}
