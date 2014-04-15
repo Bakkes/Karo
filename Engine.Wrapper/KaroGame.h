5124141cf6b22536eb3fd3135507f8efb107d6eb
chris.m@Live.nl <chris.m@live.nl>
2014-04-15 12:10:48 +0200

    Added constructor to the MoveWrapper.

---
+++ Engine.Wrapper/KaroGame.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "MoveWrapper.h"
+using namespace engine;
+
+namespace engine {
+namespace wrapper {
+	public ref class KaroGame
+	{
+		public:
+		private:
+			
+			
+	};
+}
+}
