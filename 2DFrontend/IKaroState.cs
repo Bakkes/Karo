9fda8c2c0db638901d70e99eb84c90fcc90497b4
Joep Kemperman <kazooiej@gmail.com>
2014-04-16 10:32:08 +0200

    Added KaroGameManger with state.

---
+++ 2DFrontend/IKaroState.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace _2DFrontend
+{
+	/// <summary>
+	/// Interface for karo game states. Takes a tile coordinate and
+	/// performs an action depending on the coordinate and state.
+	/// Implementations of this interface should be a singleton.
+	/// </summary>
+	interface IKaroState
+	{
+		IKaroState Instance { get; set; }
+		void Update(KaroGameManager manager, Point click);
+	}
+}
---
+++ 2DFrontend/KaroGameManager.cs
@@ -0,0 +1,25 @@
+﻿using engine.wrapper;
+
+namespace _2DFrontend
+{
+	/// <summary>
+	/// Statemachine that keeps track of the game's states.
+	/// </summary>
+	class KaroGameManager
+	{
+		/// <summary>
+		/// Current state of the game.
+		/// </summary>
+		public IKaroState CurrentState { get; set; }
+
+		/// <summary>
+		/// The current state of the current player's move.
+		/// </summary>
+		private MoveWrapper CurrentMove { get; set; }
+
+		/// <summary>
+		/// Current state of the game.
+		/// </summary>
+		private KaroGame Game { get; set; }
+	}
+}
