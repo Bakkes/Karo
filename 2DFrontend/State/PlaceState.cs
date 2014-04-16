0cee07fdcc9a0e72afa05f3aaf490e036fad51d9
Joep Kemperman <kazooiej@gmail.com>
2014-04-16 10:46:58 +0200

    Added PlaceState.

---
+++ 2DFrontend/State/PlaceState.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+
+namespace _2DFrontend.State
+{
+	/// <summary>
+	/// In this state, a human player can place a piece on an empty tile.
+	/// </summary>
+	class PlaceState : IKaroState
+	{
+		private IKaroState _instance;
+
+		public IKaroState Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new PlaceState();
+				}
+				return _instance;
+			}
+		}
+
+		/// <summary>
+		/// Private constructor for singleton.
+		/// </summary>
+		private PlaceState()
+		{
+		}
+
+		public void Update(KaroGameManager manager, Point click)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
