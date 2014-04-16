2c79ba4b3c0a75ec26c68a878e5fb50e004dab0e
Joep Kemperman <kazooiej@gmail.com>
2014-04-16 10:50:22 +0200

    Added PieceSourceState.

---
+++ 2DFrontend/State/PieceSourceState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+
+namespace _2DFrontend.State
+{
+	/// <summary>
+	/// In this state, the current player must pick one of his pieces. When
+	/// a valid piece is chosen, the game will go to the PieceDestinationState
+	/// so the player can decide where he wants to move his piece.
+	/// </summary>
+	class PieceSourceState : IKaroState
+	{
+		private PieceSourceState _instance;
+
+		public IKaroState Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new PieceSourceState();
+				}
+				return _instance;
+			}
+		}
+
+		/// <summary>
+		/// Private constructor for singleton.
+		/// </summary>
+		private PieceSourceState()
+		{
+		}
+
+		public void Update(KaroGameManager manager, Point click)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
