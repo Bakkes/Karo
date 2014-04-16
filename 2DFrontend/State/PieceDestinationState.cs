eea56277b0db521d9e0a18fcefddd6add54ad3a8
Joep Kemperman <kazooiej@gmail.com>
2014-04-16 10:53:17 +0200

    Added PieceDestinationState.

---
+++ 2DFrontend/State/PieceDestinationState.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+
+namespace _2DFrontend.State
+{
+	/// <summary>
+	/// In this state, the player will decide on which tile he wants to move
+	/// his piece to. If clicked on a non valid tile, the game will go back to
+	/// the PieceSourceState.
+	/// </summary>
+	class PieceDestinationState : IKaroState
+	{
+		private PieceDestinationState _instance;
+
+		public IKaroState Instance
+		{
+			get
+			{
+				if (_instance == null)
+				{
+					_instance = new PieceDestinationState();
+				}
+				return _instance;
+			}
+		}
+
+		/// <summary>
+		/// Private constructor for singleton.
+		/// </summary>
+		private PieceDestinationState()
+		{
+		}
+
+		public void Update(KaroGameManager manager, Point click)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
