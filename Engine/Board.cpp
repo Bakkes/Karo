5169113f58ae7860c905e346728a831f36c95465
Jappie Klooster <jappieklooster@hotmail.com>
2014-04-16 11:03:57 +0200

    added a matrix to represent the initial board state

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -42,17 +42,17 @@ namespace engine{
 		return tiles;
 	}
 	std::string Board::ToString(){
-		std::string result = "";
+		std::stringstream result;
 		_grid->TraverseTiles(
 			[&, this](Tile<int>* tile) -> void{
 				if(tile->GetPosition()->X() +1 == this->_grid->GetSize()->GetWidth()){
-					result += "\r\n";
+					result << std::endl;
 				}
 				int data = *tile->GetData();
-				result += *tile->GetData() + ",";
+				result << *tile->GetData() << ",";
 			}
 		);
-		return result;
+		return result.str();
 		
 	}
 	Tile<int>* Board::GetRelativeTileAt(const Vector2D& relativePosition) const{
