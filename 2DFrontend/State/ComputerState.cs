66aee9a23b535603e56dcc38344f76101d7e12e2
Joep Kemperman <kazooiej@gmail.com>
2014-04-17 12:56:53 +0200

    Cleaned up usings.

--- 2DFrontend/State/ComputerState.cs
+++ 2DFrontend/State/ComputerState.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Drawing;
 
 namespace _2DFrontend.State
--- 2DFrontend/State/PieceSourceState.cs
+++ 2DFrontend/State/PieceSourceState.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using System.Drawing;
-using engine.wrapper;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using engine.wrapper;
 
 namespace _2DFrontend.State
 {
