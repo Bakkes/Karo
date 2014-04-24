de39d882b9fbb0d4c5952b30be2fb92057255f29
Johnnei <johnnei07@live.nl>
2014-04-24 15:45:11 +0200

    Marked all getters in move constant

--- Engine/Move.h
+++ Engine/Move.h
@@ -15,15 +15,15 @@ namespace engine {
 		// Creates either a MOVE or JUMP move which DOES change the board structure
 		Move(MoveType type, Vector2D fromCell, Vector2D toCell, Vector2D usedCell);
 		// The type of the move
-		MoveType GetMoveType();
+		MoveType GetMoveType() const;
 		// The location from which the piece will come from
-		Vector2D GetFromCell();
+		Vector2D GetFromCell() const;
 		// The location to which the piece will go to
-		Vector2D GetToCell();
+		Vector2D GetToCell() const;
 		// The location of the tile used to jump to (OPTIONAL: Test with HasUsedCell to see if it is set!)
-		Vector2D GetUsedCell();
+		Vector2D GetUsedCell() const;
 		// If a tile was moved to make this move possible
-		bool HasUsedCell();
+		bool HasUsedCell() const;
 
 	private:
 		// The type of the move
