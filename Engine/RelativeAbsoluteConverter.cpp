a601980f8fed7ee4e01f1fcf5cc9820c4422e97d
Jappie Klooster <jappieklooster@hotmail.com>
2014-05-06 16:09:52 +0200

    added initial relative converter

---
+++ Engine/RelativeAbsoluteConverter.cpp
@@ -0,0 +1,58 @@
+#include "RelativeAbsoluteConverter.h"
+#include "Grid.h"
+namespace engine{
+
+	RelativeAbsoluteConverter::RelativeAbsoluteConverter(Grid* parsable){
+		Vector2D _topLeft = Vector2D();
+		_rowTileCount = vector<int>();
+		_colTileCount = vector<int>();
+		for(unsigned i = 0; i < parsable->GetSize()->GetWidth(); i++){
+			_rowTileCount.push_back(int(0));
+		}
+		for(unsigned i = 0; i < parsable->GetSize()->GetHeight(); i++){
+			_colTileCount.push_back(int(0));
+		}
+		parsable->TraverseCells(
+			[this](Cell<int>* tile) -> void {
+				if(tile->GetData() & HasTile){
+					this->_rowTileCount.at(tile->GetPosition().X())+=1;
+					this->_rowTileCount.at(tile->GetPosition().Y())+=1;
+				}
+			}
+		);
+	}
+	Vector2D RelativeAbsoluteConverter::ToRelative(const Vector2D& input)const{
+		return WrapArround(input - _topLeft);
+	}
+	Vector2D RelativeAbsoluteConverter::ToAbsolute(const Vector2D& input)const{
+		return WrapArround(input + _topLeft);
+	}
+
+	Vector2D& RelativeAbsoluteConverter::WrapArround(Vector2D& input)const{
+		if(input.X() < 0){
+			input.X(input.X() + GetWidth());
+		}
+		if(input.Y() < 0){
+			input.Y(input.Y() + GetHeight());
+		}
+		if(input.X() >= GridWidth()){
+			input.X(0);
+		}
+		if(input.Y() >= GridHeight()){
+			input.Y(0);
+		}
+		return input;
+	}
+	// allows the converter to be kept up to date with board
+	void RelativeAbsoluteConverter::MoveTile(const Vector2D& from, const Vector2D& to){
+		_rowTileCount.at(from.X());
+	}
+
+
+	int RelativeAbsoluteConverter::GridWidth()const{
+		return _colTileCount.size();
+	}
+	int RelativeAbsoluteConverter::GridHeight()const{
+		return _rowTileCount.size();
+	}
+}
---
+++ Engine/RelativeAbsoluteConverter.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "IBoard.h"
+#include "Size.h"
+namespace engine{
+	class Grid;
+	class RelativeAbsoluteConverter{
+	public:
+		RelativeAbsoluteConverter(Grid* parseable);
+		~RelativeAbsoluteConverter();
+		Vector2D ToRelative(const Vector2D&)const;
+		Vector2D ToAbsolute(const Vector2D&)const; 
+		// allows the converter to be kept up to date with board
+		void MoveTile(const Vector2D& from, const Vector2D& to);
+	private:
+		// the absolute topleft
+		Vector2D _topLeft;
+
+		// amount of tiles per row
+		vector<int> _rowTileCount;
+
+		// amount of tiles per collumn
+		vector<int> _colTileCount;
+		Vector2D& WrapArround(Vector2D& input) const;
+
+		int GridWidth() const;
+		int GridHeight() const;
+	};
+}
