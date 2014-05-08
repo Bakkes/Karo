037bee41f3038e7fbef918f4afcbd500fd5ca123
Johnnei <johnnei07@live.nl>
2014-05-08 11:05:34 +0200

    Added more logging information

--- Engine/ComputerPlayerAB.cpp
+++ Engine/ComputerPlayerAB.cpp
@@ -1,6 +1,7 @@
 #include "ComputerPlayerAB.h"
 #include "ComputerPlayerUtils.h"
 #include <Windows.h>
+#include <sstream>
 
 namespace engine {
 	/*
@@ -22,7 +23,32 @@ namespace engine {
 		return MinimaxStep(player, 0, EvalResult(INT_MIN, INT_MAX)).GetMove();
 	}
 
+	void printInteger(int i) {
+
+		stringstream string;
+		string << i;
+		OutputDebugString(string.str().c_str());
+	}
+
+	void printPlayer(Players player) {
+		if (player == Max) {
+			OutputDebugStringW(L"Max");
+		} else {
+			OutputDebugStringW(L"Min");
+		}
+	}
+
 	EvalResult& ComputerPlayerAB::MinimaxStep(Players player, int depth, EvalResult& result) {
+		OutputDebugStringW(L"MinimaxStep(");
+		printPlayer(player);
+		OutputDebugStringW(L", ");
+		printInteger(depth);
+		OutputDebugStringW(L", bestForMax=");
+		printInteger(result.GetBestForMax());
+		OutputDebugStringW(L", bestForMin=");
+		printInteger(result.GetBestForMin());
+		OutputDebugStringW(L")\n");
+
 		std::vector<Move>* possibleMoves = _board->GetLegalMoves(player);
 		for (auto it = possibleMoves->begin(); it != possibleMoves->end(); ++it) {
 			Move move = (*it);
@@ -30,8 +56,18 @@ namespace engine {
 			EvalResult score = GetScore(player, move, depth, result);
 			_board->ExecuteMove(&ComputerPlayerUtils::InvertMove(move), player);
 
+			OutputDebugStringW(L"MoveCompare: Player=");
+			printPlayer(player);
+			OutputDebugStringW(L", Score=");
+			printInteger(score.GetScore());
+			OutputDebugStringW(L", bestForMax=");
+			printInteger(result.GetBestForMax());
+			OutputDebugStringW(L", bestForMin=");
+			printInteger(result.GetBestForMin());
+			OutputDebugStringW(L"\n");
+
 			if (player == Max) {
-				if (score.GetScore() <= result.GetBestForMax() && result.GetBestForMax() != INT_MIN) {
+				if (score.GetScore() <= result.GetBestForMin() && result.GetBestForMin() != INT_MAX) {
 					// Cut off
 					OutputDebugStringW(L"Max Node Cut off\n");
 					break;
@@ -41,7 +77,7 @@ namespace engine {
 					result.SetScore(score.GetScore());
 				}
 			} else  if (player == Min)  {
-				if (score.GetScore() <= result.GetBestForMin() && result.GetBestForMin() != INT_MAX) {
+				if (score.GetScore() <= result.GetBestForMax() && result.GetBestForMax() != INT_MIN) {
 					// Cut off
 					OutputDebugStringW(L"Min Node Cut off\n");
 					break;
@@ -65,10 +101,25 @@ namespace engine {
 			// We are allowed to go deeper, take the result of the next step
 			EvalResult score = MinimaxStep(ComputerPlayerUtils::InvertPlayer(player), depth + 1, result);
 
+			OutputDebugStringW(L"RET MinimaxStep(");
+			if (player == Max) {
+				OutputDebugStringW(L"Max");
+			} else {
+				OutputDebugStringW(L"Min");
+			}
+			OutputDebugStringW(L", ");
+			printInteger(depth);
+			OutputDebugStringW(L", bestForMax=");
+			printInteger(result.GetBestForMax());
+			OutputDebugStringW(L", bestForMin=");
+			printInteger(result.GetBestForMin());
+			OutputDebugStringW(L")\n");
+
+
 			// Propogade the values up tree -> So swap them
-			int temp = score.GetBestForMax();
-			score.SetBestForMax(score.GetBestForMin());
-			score.SetBestForMin(temp);
+			int newMax = result.GetBestForMax();
+			result.SetBestForMax(result.GetBestForMin());
+			result.SetBestForMin(newMax);
 
 			OutputDebugStringW(L"Propogading Score\n");
 			return score;
@@ -77,7 +128,9 @@ namespace engine {
 			EvalResult score(result.GetBestForMax(), result.GetBestForMin());
 			score.SetMove(move);
 			score.SetScore(_evaluator->Eval(_board, player));
-			OutputDebugStringW(L"Eval Score\n");
+			OutputDebugStringW(L"Eval Score: ");
+			printInteger(score.GetScore());
+			OutputDebugStringW(L"\n");
 			
 			return score;
 		}
