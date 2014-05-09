324d25104237cb45c112cedb1006a1834053909f
leander <leandernieuwenstein@hotmail.com>
2014-05-09 14:24:54 +0200

    changed those stupid batch files

--- Engine/postbuild.release.bat
+++ Engine/postbuild.release.bat
@@ -1,2 +1,3 @@
 echo F|xcopy "..\Release\Engine.dll" "..\2DFrontend\bin\Release" /y
-echo F|xcopy "..\Release\Engine.dll" "..\XNAFrontend\bin\Release" /y
+
+echo F|xcopy "..\Release\Engine.dll" "..\XNAFrontend\XNAFrontend\bin\Release" /y
