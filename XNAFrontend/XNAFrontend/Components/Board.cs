87b3d4fa65e9a2452adfc4eb8a2a892aa0388a59
Joep Kemperman <kazooiej@gmail.com>
2014-05-09 10:21:22 +0200

    Added a board component to the XNA frontend.

---
+++ XNAFrontend/XNAFrontend/Components/Board.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace XNAFrontend.Components
+{
+	/// <summary>
+	/// 3D board representation of the karo board game.
+	/// </summary>
+	internal class Board : DrawableGameComponent
+	{
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+		}
+	}
+}
