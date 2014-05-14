a0561f8791d86bff073f7831d9c5aafd97c6da9f
Joep Kemperman <mail@joepkemperman.nl>
2014-05-14 15:40:10 +0200

    Made the board smaller (easier for debugging.)

--- 2DFrontend/KaroPanel.cs
+++ 2DFrontend/KaroPanel.cs
@@ -27,7 +27,7 @@ namespace _2DFrontend
 		/// <summary>
 		/// Color of the circle accent on pieces.
 		/// </summary>
-		private Pen _pieceAccentColor = new Pen(Color.FromArgb(34, 34, 34), 5);
+		private Pen _pieceAccentColor = new Pen(Color.FromArgb(34, 34, 34), 2);
 
 		/// <summary>
 		/// Color of max's pieces.
@@ -44,16 +44,16 @@ namespace _2DFrontend
 		/// <summary>
 		/// Width/height of the tiles in pixels.
 		/// </summary>
-		private const int CellSize = 50;
+		private const int CellSize = 25;
 
-		private const int AccentSize = 30;
+		private const int AccentSize = 15;
 
-		private const int PieceSize = 40;
+		private const int PieceSize = 20;
 
 		/// <summary>
 		/// Gap left and right of every tile.
 		/// </summary>
-		private const int Gap = 3;
+		private const int Gap = 2;
 
 		public KaroPanel()
 			: base()
