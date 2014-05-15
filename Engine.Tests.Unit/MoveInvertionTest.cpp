32140d5acf57837bf68b53393fb32100d3a7c6c9
Joep Kemperman <mail@joepkemperman.nl>
2014-05-15 14:27:22 +0200

    Renamed the test BUGTEST to a more descriptive name.

--- Engine.Tests.Unit/MoveInvertionTest.cpp
+++ Engine.Tests.Unit/MoveInvertionTest.cpp
@@ -431,7 +431,7 @@ namespace Tests {
 			);
 		}
 
-		TEST_METHOD(BUGTEST) {
+		TEST_METHOD(UndoMoveDoesNotCreateInvalidBoardState) {
 			string expected =
 				"3,3,1,3,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,\n"
 				"3,5,1,5,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,\n"
