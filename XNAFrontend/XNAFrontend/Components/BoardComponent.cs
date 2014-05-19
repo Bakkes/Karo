1f487648d684ae04304dcc670cadbb778f0374e2
chris.m@Live.nl <chris.m@live.nl>
2014-05-19 14:01:13 +0200

    Started working on rays

--- XNAFrontend/XNAFrontend/Components/BoardComponent.cs
+++ XNAFrontend/XNAFrontend/Components/BoardComponent.cs
@@ -12,6 +12,9 @@ namespace XNAFrontend.Components
 	/// </summary>
     internal class Board : ACommonComponent
 	{
+        const float SIZE = 1f;
+        const float GAP = 0.1f;
+
 		private Model _tileModel;
 	
 		private KaroGameManager KaroGameManager
@@ -44,8 +47,83 @@ namespace XNAFrontend.Components
 
 		public override void Update(GameTime gameTime)
 		{
+            
+            MouseState mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                
+
+                ICamera camera = (ICamera)Game.Services.GetService(typeof(ICamera));
+                int mouseX = mouseState.X;
+                int mouseY = mouseState.Y;
+                Vector3 nearsource = new Vector3((float)mouseX, (float)mouseY, 0f);
+                Vector3 farsource = new Vector3((float)mouseX, (float)mouseY, 1f);
+
+                Matrix world = Matrix.CreateTranslation(0, 0, 0);
+
+                Vector3 nearPoint = GraphicsDevice.Viewport.Unproject(nearsource,
+                    camera.Projection, camera.View, world);
+
+                Vector3 farPoint = GraphicsDevice.Viewport.Unproject(farsource,
+                    camera.Projection, camera.View, world);
+
+                Vector3 direction = farPoint - nearPoint;
+                direction.Normalize();
+                Ray pickRay = new Ray(nearPoint, direction);
+
+                for (int x = 0; x <= 20; x++)
+                {
+                    for (int y = 0; y <= 20; y++)
+                    {
+                        CellWrapper cell = KaroGameManager.Board.GetRelativeCellAt(new Vector2DWrapper(x, y));
+                        if (cell.HasTile())
+                        {
+                            BoundingBox b = UpdateBoundingBox(_tileModel, world * Matrix.CreateTranslation(new Vector3(x * (SIZE + GAP), 0, y * (SIZE + GAP))));
+                            if (pickRay.Intersects(b) > 0)
+                            {
+                                System.Console.WriteLine("Found at " + x + ", " + y + ": " + pickRay.Intersects(b));
+                            }
+
+                        }
+                    }
+                }
+            }
 			base.Update(gameTime);
 		}
+
+        protected BoundingBox UpdateBoundingBox(Model model, Matrix worldTransform)
+        {
+            // Initialize minimum and maximum corners of the bounding box to max and min values
+            Vector3 min = new Vector3(float.MaxValue, float.MaxValue, float.MaxValue);
+            Vector3 max = new Vector3(float.MinValue, float.MinValue, float.MinValue);
+
+            // For each mesh of the model
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                {
+                    // Vertex buffer parameters
+                    int vertexStride = meshPart.VertexBuffer.VertexDeclaration.VertexStride;
+                    int vertexBufferSize = meshPart.NumVertices * vertexStride;
+
+                    // Get vertex data as float
+                    float[] vertexData = new float[vertexBufferSize / sizeof(float)];
+                    meshPart.VertexBuffer.GetData<float>(vertexData);
+
+                    // Iterate through vertices (possibly) growing bounding box, all calculations are done in world space
+                    for (int i = 0; i < vertexBufferSize / sizeof(float); i += vertexStride / sizeof(float))
+                    {
+                        Vector3 transformedPosition = Vector3.Transform(new Vector3(vertexData[i], vertexData[i + 1], vertexData[i + 2]), worldTransform);
+
+                        min = Vector3.Min(min, transformedPosition);
+                        max = Vector3.Max(max, transformedPosition);
+                    }
+                }
+            }
+
+            // Create and return bounding box
+            return new BoundingBox(min, max);
+        }
 
 		public override void Draw(GameTime gameTime)
 		{
@@ -78,8 +156,6 @@ namespace XNAFrontend.Components
 		/// </summary>
 		private void DrawCellAt(CellWrapper cell, int x, int y)
 		{
-			const float SIZE = 1f;
-			const float GAP = 0.1f;
 			ICamera camera = (ICamera)Game.Services.GetService(typeof(ICamera));
 			Matrix world = Matrix.CreateRotationX(MathHelper.ToRadians(-90));
 			foreach (ModelMesh mesh in _tileModel.Meshes)
