e5c29e23fdb37eda78a9fdd74dd44e718c937db9
Joep Kemperman <mail@joepkemperman.nl>
2014-05-20 11:03:15 +0200

    Spacs -> tabs.

--- XNAFrontend/XNAFrontend/Services/ICamera.cs
+++ XNAFrontend/XNAFrontend/Services/ICamera.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 
 namespace XNAFrontend.Services
 {
-    interface ICamera
-    {
+	interface ICamera
+	{
 		Vector3 Position { get; }
 		Matrix Projection { get; }
 		Matrix View { get; }
-    }
+	}
 }
