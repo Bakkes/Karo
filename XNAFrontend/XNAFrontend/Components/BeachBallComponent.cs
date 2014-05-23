9b2a8ec7b1e3097596466d805155bb0c1f0ef476
Joep Kemperman <mail@joepkemperman.nl>
2014-05-23 15:57:47 +0200

    Added a BeachBallComponent.

---
+++ XNAFrontend/XNAFrontend/Components/BeachBallComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace XNAFrontend.Components
+{
+	public class BeachBallComponent : ACommonComponent
+	{
+		public BeachBallComponent(KaroGame game)
+			: base(game)
+		{
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		public override void Draw(GameTime gameTime)
+		{
+			base.Draw(gameTime);
+		}
+	}
+}
