612b68c53f0a544803c6746f750638f94a9b6fc6
Johnnei <johnnei07@live.nl>
2014-05-26 11:54:04 +0200

    Fixed the whitespace issues in ZobristHashing

--- Engine/ZobristHashing.cpp
+++ Engine/ZobristHashing.cpp
@@ -1,69 +1,42 @@
 
 #include "ZobristHashing.h"
 
-using namespace stdext;
-
 namespace engine{
 
-	ZobristHashing::ZobristHashing(void)
-	{
+	ZobristHashing::ZobristHashing(void) {
 		init_zobrist();
 	}
 
-	ZobristHashing::~ZobristHashing(void)
-	{
+	ZobristHashing::~ZobristHashing(void) {
 	}
 
-	void ZobristHashing::init_zobrist()
-	{
+	void ZobristHashing::init_zobrist() {
 		srand((unsigned)time(NULL));
 	
-		for(int i=0;i<16;i++)
-		{
-			for(int j=0; j<400; j++)
-			{
-				table[i][j] = rand()%1000;
+		for(int i = 0; i < 16; i++) {
+			for(int j = 0; j < 400; j++) {
+				table[i][j] = rand() % 1000;
 			}
 		}
-
-
 	}
 
-	void ZobristHashing::board_hash()
-	{
-
-
-
-
-
-
-
+	void ZobristHashing::board_hash() {
 	}
 
-	int ZobristHashing::hash(Board a) 
-	{
-		int h =0;
+	int ZobristHashing::hash(Board a) {
+		int h = 0;
 
-		for(int i=0; i<20;i++)
-		{
-				for(int k=0; k<20; k++)
-			{
-				Cell<int>* j=a.GetAbsoluteCellAt(Vector2D(i,k));
-				int value = (int)j->GetPosition().X() + (int)j->GetPosition().Y()*20;
+		for(int i = 0; i < 20; i++) {
+			for(int k = 0; k < 20; k++) {
+				Cell<int>* j = a.GetAbsoluteCellAt(Vector2D(i, k));
+				int value = (int)j->GetPosition().X() + (int)j->GetPosition().Y() * 20;
 
-				if(j->GetData() != 2)
-				{
-					h=h^table[j->GetData()][value];
-
+				if(j->GetData() != 2) {
+					h = h^table[j->GetData()][value];
 				}
-
 			}
 		}
 		return h;
-		
-
 	}
-
-
 
 }
