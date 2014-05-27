5ba8d227cb7096a104ca1b909c82f465a269d171
Joep Kemperman <mail@joepkemperman.nl>
2014-05-27 12:19:08 +0200

    CurrentMove is resetted.

--- KaroManager/KaroComputerManager.cs
+++ KaroManager/KaroComputerManager.cs
@@ -18,6 +18,7 @@ namespace KaroManager
 			Debug.WriteLine("TopLeft: {0}", Board.GetRelativeCellAt(new Vector2DWrapper(0, 0)).GetAbsolutePosition());
 			Debug.WriteLine("Before Execute Board State: {0}", Board.ToString());
 			base.ExecuteMove(move);
+			CurrentMove = null;
 			ChangeState(ComputerState.Instance);
 			Debug.WriteLine("After Board State: {0}", Board.ToString());
 		}
