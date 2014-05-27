ea48bfbf9c1671ef62dc26c84893706f644053cb
Johnnei <johnnei07@live.nl>
2014-05-27 16:05:05 +0200

    Started with the extension

--- Engine/TranspositionExtension.cpp
+++ Engine/TranspositionExtension.cpp
@@ -1,12 +1,20 @@
 #include "TranspositionExtension.h"
+#include "RngTimeBased.h"
 
 namespace engine {
 
 	TranspositionExtension::TranspositionExtension() {
+		IRng* rand = new RngTimeBased();
+
+		delete rand;
 	}
 
 
 	TranspositionExtension::~TranspositionExtension()
 	{
+	}
+
+	bool TranspositionExtension::ShouldContinue(const IBoard* board, const EvalResult& currentResult, EvalResult& prevResult, const Players& player) {
+		return true;
 	}
 }
