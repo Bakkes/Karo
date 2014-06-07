256ba94e4e4ae7f2a73a8198ba39a91533240a1a
Joep Kemperman <mail@joepkemperman.nl>
2014-06-07 20:39:56 +0200

    Added GMP library.

--- Engine/postbuild.debug.bat
+++ Engine/postbuild.debug.bat
@@ -3,16 +3,10 @@
 FOR /F "tokens=3 delims= " %%G in ('reg query "hklm\system\controlset001\control\nls\language" /v Installlanguage') DO (
 IF [%%G] EQU [0413] (
 	echo B|xcopy "..\Debug\Engine.dll" "..\2DFrontend\bin\Debug\Engine.dll" /y
-
-
-
 	echo B|xcopy "..\Debug\Engine.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Debug\Engine.dll" /y
-
+	echo B|xcopy "..\GMP\gmp.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Debug\gmp.dll" /y
 ) ELSE (
 	echo F|xcopy "..\Debug\Engine.dll" "..\2DFrontend\bin\Debug\Engine.dll" /y
-
-
-
 	echo F|xcopy "..\Debug\Engine.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Debug\Engine.dll" /y
-
+	echo F|xcopy "..\GMP\gmp.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Debug\gmp.dll" /y
 ))
--- Engine/postbuild.release.bat
+++ Engine/postbuild.release.bat
@@ -3,16 +3,10 @@
 FOR /F "tokens=3 delims= " %%G in ('reg query "hklm\system\controlset001\control\nls\language" /v Installlanguage') DO (
 IF [%%G] EQU [0413] (
 	echo B|xcopy "..\Release\Engine.dll" "..\2DFrontend\bin\Release\Engine.dll" /y
-
-
-
 	echo B|xcopy "..\Release\Engine.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Release\Engine.dll" /y
-
+	echo B|xcopy "..\GMP\gmp.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Release\gmp.dll" /y
 ) ELSE (
 	echo F|xcopy "..\Release\Engine.dll" "..\2DFrontend\bin\Release\Engine.dll" /y
-
-
-
 	echo F|xcopy "..\Release\Engine.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Release\Engine.dll" /y
-
+	echo F|xcopy "..\GMP\gmp.dll" "..\XNAFrontend\XNAFrontend\bin\x86\Release\gmp.dll" /y
 ))
