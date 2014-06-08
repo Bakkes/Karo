b05817205be6075f7a131ae4fd22766ca5b6d44b
Johnnei <johnnei07@live.nl>
2014-06-08 17:24:40 +0200

    Corrected the BigInteger comparision overloads

--- Engine/BigInteger.h
+++ Engine/BigInteger.h
@@ -24,8 +24,7 @@ namespace engine {
 		friend bool operator == (const BigInteger& lhs, const BigInteger& rhs) {
 			BigInteger c(lhs);
 			mpz_sub(c._value, c._value, lhs._value);
-			int result = mpz_sgn(c._value);
-			return result == 0;
+			return mpz_sgn(c._value) == 0;
 		}
 
 		friend bool operator != (const BigInteger& lhs, const BigInteger& rhs) {
@@ -33,19 +32,27 @@ namespace engine {
 		}
 
 		friend bool operator < (const BigInteger& lhs, const BigInteger& rhs) {
-			return mpz_cmp(lhs._value, rhs._value) < 0;
+			BigInteger c(lhs);
+			mpz_sub(c._value, c._value, lhs._value);
+			return mpz_sgn(c._value) < 0;
 		}
 
 		friend bool operator > (const BigInteger& lhs, const BigInteger& rhs) {
-			return mpz_cmp(lhs._value, rhs._value) > 0;
+			BigInteger c(lhs);
+			mpz_sub(c._value, c._value, lhs._value);
+			return mpz_sgn(c._value) > 0;
 		}
 
 		friend bool operator <= (const BigInteger& lhs, const BigInteger& rhs) {
-			return mpz_cmp(lhs._value, rhs._value) <= 0;
+			BigInteger c(lhs);
+			mpz_sub(c._value, c._value, lhs._value);
+			return mpz_sgn(c._value) <= 0;
 		}
 
 		friend bool operator >= (const BigInteger& lhs, const BigInteger& rhs) {
-			return mpz_cmp(lhs._value, rhs._value) >= 0;
+			BigInteger c(lhs);
+			mpz_sub(c._value, c._value, lhs._value);
+			return mpz_sgn(c._value) >= 0;
 		}
 
 	private:
